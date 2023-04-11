b38c6df5966873c99489cb6bf727cd65d48bcd9c
Saiful islam <saiful.bijulia@gmail.com>
2023-04-11 12:15:39 +0530

    project done

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-let btnTranslate = document.querySelector("#btn-translate");
+const translateButton = document.querySelector("#btn-translate"); // translate button
+const translateInput = document.querySelector("#input"); //target input
+const translateOutput = document.querySelector("#output");// target output
 
+// API url
+const url = `https://api.funtranslations.com/translate/minion.json`;
+
+// click handler function
 function clickHandle() {
-    console.log("clicked!");
+    let input = translateInput.value;
+    let finalUrl = constructURL(input);
+    fetch(finalUrl)
+        .then(res => res.json())
+        .then(json => { translateOutput.innerText = json.contents.translated })
+    .catch(()=>alert("something went wrong with server. please try again later"))
 }
-btnTranslate.addEventListener("click", clickHandle)
+
+// click event listener
+translateButton.addEventListener("click", clickHandle)
+
+// converto input text to encodeUri
+function constructURL(inputText) {
+    var encodedURI = encodeURI(inputText);
+    return `${url}?text=${encodedURI}`;
+}
